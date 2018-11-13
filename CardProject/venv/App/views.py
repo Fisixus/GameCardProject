535cada1b2a356c3fb33c84a22beedf71f6ee116
doruksahin <doruksahin98@gmail.com>
2018-11-13 21:18:43 +0300

    Extension yuklenemedigi icin sepete ekleme cikarmadogru calismiyor.

--- CardProject/venv/App/views.py
+++ CardProject/venv/App/views.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, g, session, url_for, redirect
+from jinja2 import Environment
+
 import psycopg2 as sql
 import os
 import psycopg2.errorcodes
@@ -10,13 +12,20 @@ psycopg2.errorcodes.UNIQUE_VIOLATION
 app = Flask(__name__, template_folder='Templates')
 
 DATABASE_URL = os.environ['DATABASE_URL']
+jinja_env = Environment(extensions=['jinja2.ext.do'])
+#jinja2.Export('jinja_env')
 app.secret_key = b'_1#y2L"F4Q8z\n\xec]/'
-shopping_list = [] # Alisveris listesi olusturulabilir.
+
+
+
+
+shopping_list = []
 
 def connect_db():
     con = sql.connect(DATABASE_URL)
     con.cursor_factory = psycopg2.extras.NamedTupleCursor
     return con
+
 
 @app.before_request
 def before_request():
@@ -28,48 +37,77 @@ def teardown_request(exception):
     if hasattr(g, 'db'):
         g.db.close()
 
+
 #Ana sayfa
 @app.route('/listall')
 def listall():
-   #if 'user_email' not in session.keys():
-     #   return redirect('login')
-   con = connect_db()
-   
-   cur = con.cursor()
-   query =  "SELECT DISTINCT * FROM PRODUCT ORDER BY Name "
-   cur.execute(query)
+    global shopping_list
+    #if 'user_email' not in session.keys():
+    #   return redirect('login')
 
-   rows = cur.fetchall()
-   con.close()
-   return render_template('listall.html', rows=rows, shopping_list=shopping_list)
+    con = connect_db()
+    cur = con.cursor()
+    query =  "SELECT DISTINCT * FROM PRODUCT ORDER BY Name "
+    cur.execute(query)
+    rows = cur.fetchall()
+    con.close()
+
+
+    dummy = request.args.get('empty')
+    if(dummy is not None):
+        print("sasasasa\nsasasasasas\n------\n")
+        shopping_list = []
+    return render_template('listall.html', rows=rows, shopping_list=shopping_list)
+
 
 @app.route('/listgamecards')
 def listgamecards():
-   #if 'user_email' not in session.keys():
-     #   return redirect('login')
-   con = connect_db()
+    global shopping_list
+    #if 'user_email' not in session.keys():
+    #   return redirect('login')
 
-   cur = con.cursor()
-   query =  "SELECT DISTINCT * FROM PRODUCT,GAMECARD WHERE id = pid ORDER BY Name "
-   cur.execute(query)
+    con = connect_db()
+    cur = con.cursor()
+    query =  "SELECT DISTINCT * FROM PRODUCT,GAMECARD WHERE id = pid ORDER BY Name "
+    cur.execute(query)
+    rows = cur.fetchall()
+    con.close()
 
-   rows = cur.fetchall()
-   con.close()
-   return render_template('listgamecards.html', rows=rows, shopping_list=shopping_list)
+    dummy = request.args.get('empty')
+    if(dummy is not None):
+        print("sasasasa\nsasasasasas\n------\n")
+        shopping_list = []
+    return render_template('listgamecards.html', rows=rows, shopping_list=shopping_list)
+
 
 @app.route('/listboardgames')
 def listboardgames():
-   #if 'user_email' not in session.keys():
-     #   return redirect('login')
-   con = connect_db()
+    global shopping_list
+    #if 'user_email' not in session.keys():
+    #   return redirect('login')
 
-   cur = con.cursor()
-   query =  "SELECT DISTINCT * FROM PRODUCT,BOARDGAME WHERE id = pid ORDER BY Name "
-   cur.execute(query)
+    con = connect_db()
+    cur = con.cursor()
+    query =  "SELECT DISTINCT * FROM PRODUCT,BOARDGAME WHERE id = pid ORDER BY Name "
+    cur.execute(query)
+    rows = cur.fetchall()
+    con.close()
 
-   rows = cur.fetchall()
-   con.close()
-   return render_template('listboardgames.html', rows=rows, shopping_list=shopping_list)
+    dummy = request.args.get('empty')
+    if(dummy is not None):
+        print("sasasasa\nsasasasasas\n------\n")
+        shopping_list = []
+    return render_template('listboardgames.html', rows=rows, shopping_list=shopping_list)
+
+
+
+
+
+
+
+
+
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -90,6 +128,7 @@ def login():
             return render_template('login.html')
     else:
         return render_template('login.html')
+
 
 @app.route('/logout')
 def logout():
@@ -119,8 +158,38 @@ def cargo():
 
 @app.route('/payment')
 def payment():
+
+    con = connect_db()
+    cur = con.cursor()
+    '''
+    shopping_dict_list = []
+    for item in shopping_list:
+        print("item: {}\n".format(item))
+        query =  'SELECT DISTINCT NAME, PRICE FROM PRODUCT,BOARDGAME WHERE id = pid AND name="{}" ORDER BY Name'.format(item)
+        cur.execute(query)
+        _dict = {}
+        _dict['name'] = cur.fetchall()[0]
+        _dict['price'] = cur.fetchall()[1]
+        shopping_dict_list.append(_dict)
+    con.close()
+    '''
+    shopping_dict_list = [
+        {
+            'name': 'item1',
+            'price': 5
+        },
+        {
+            'name': 'item2',
+            'price': 10
+        },
+        {
+            'name': 'item3',
+            'price': 15
+        }
+    ]
+
     cargo_name = request.args.get('cargo_name')
-    return render_template("payment.html", cargo_name=cargo_name, shopping_list=shopping_list)
+    return render_template("payment.html", cargo_name=cargo_name, shopping_dict_list=shopping_dict_list)
 
 
 if __name__ == '__main__':
