b7df56804b44cb729349b53e93731f63cb8ef896
doruksahin <doruksahin98@gmail.com>
2018-11-13 15:54:46 +0300

    Sepete ekleme secenegi eklendi.

--- CardProject/venv/App/views.py
+++ CardProject/venv/App/views.py
@@ -11,6 +11,7 @@ app = Flask(__name__, template_folder='Templates')
 
 DATABASE_URL = os.environ['DATABASE_URL']
 app.secret_key = b'_1#y2L"F4Q8z\n\xec]/'
+shopping_list = [] # Alisveris listesi olusturulabilir.
 
 def connect_db():
     con = sql.connect(DATABASE_URL)
@@ -40,7 +41,7 @@ def listall():
 
    rows = cur.fetchall()
    con.close()
-   return render_template('listall.html', rows=rows)
+   return render_template('listall.html', rows=rows, shopping_list=shopping_list)
 
 @app.route('/listgamecards')
 def listgamecards():
@@ -54,7 +55,7 @@ def listgamecards():
 
    rows = cur.fetchall()
    con.close()
-   return render_template('listgamecards.html', rows=rows)
+   return render_template('listgamecards.html', rows=rows, shopping_list=shopping_list)
 
 @app.route('/listboardgames')
 def listboardgames():
@@ -68,7 +69,7 @@ def listboardgames():
 
    rows = cur.fetchall()
    con.close()
-   return render_template('listboardgames.html', rows=rows)
+   return render_template('listboardgames.html', rows=rows, shopping_list=shopping_list)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -97,29 +98,6 @@ def logout():
     return redirect(url_for('login'))
 
 
-
-item_table = [
-    {
-        'Name': 'Yugioh',
-        'Price': 55,
-        'SourceOfSupply': 'www.hepsiburada.com',
-        'NumberOfProduct': 5
-    },
-    {
-        'Name': 'Final Fantasy',
-        'Price': 100,
-        'SourceOfSupply': 'www.hepsiburada.com',
-        'NumberOfProduct': 10
-    },
-    {
-        'Name': 'Dj Dikkat',
-        'Price': 5,
-        'SourceOfSupply': 'www.mardatoni.com',
-        'NumberOfProduct': 1
-    },
-
-]
-
 cargo_companies = [
     {
         'Name': 'Yurtici Kargo',
@@ -141,8 +119,6 @@ cargo_companies = [
 
 
 
-shopping_list = {} # Alisveris listesi olusturulabilir.
-
 
 @app.route('/cargo')
 def cargo():
@@ -156,21 +132,20 @@ def cargo():
     item_dict = cur.fetchall()[0]
     con.close()
 
-    return render_template("cargo.html", item_dict=item_dict, cargo_companies=cargo_companies)
+    return render_template("cargo.html", item_dict=item_dict, cargo_companies=cargo_companies, shopping_list=shopping_list)
 
 
 @app.route('/payment')
 def payment():
-    item_name = request.args.get('item_name') # Alinan argumanlar her zaman string
-    item_price = request.args.get('item_price')
-    cargo = request.args.get('cargo')
+    pprint(shopping_list)
+    #cargo = request.args.get('cargo')
 
     for company in cargo_companies:
         if company['Name'] == cargo:
             cargo_price = company['Price']
 
-    total_price = float(item_price) + cargo_price
-    return render_template("payment.html", item_name=item_name, total_price=total_price)
+    #total_price = float(item_price) + cargo_price
+    return render_template("payment.html", shopping_list=shopping_list)
     # payment.html'ye kart mi kutu mu bilgisi goturulup 'yugioh kartiniz', 'monopoly kutu oyununuz' gibi ifadeler kullanilabilir.
 
 
