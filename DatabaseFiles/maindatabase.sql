7e96078c248cd46cb8b827287ab88ede83ecea3f
Furkan Dinç <36295973+furkandinc@users.noreply.github.com>
2018-11-14 13:17:58 +0300

    Update maintabase.sql
    Formatted whitespaces and shoppinglistitem table added.

--- DatabaseFiles/maindatabase.sql
+++ DatabaseFiles/maindatabase.sql
@@ -1,67 +1,76 @@
 CREATE TABLE CUSTOMER (
-    Id INTEGER NOT NULL,
-    Email VARCHAR(32) NOT NULL UNIQUE,
-    Address VARCHAR(64),
-    Fname VARCHAR(32),
-    Lname VARCHAR(32),
-    Password VARCHAR(32) NOT NULL,
-    PRIMARY KEY(Id)
+	Id INTEGER NOT NULL,
+	Email VARCHAR(32) NOT NULL UNIQUE,
+	Address VARCHAR(64),
+	Fname VARCHAR(32),
+	Lname VARCHAR(32),
+	Password VARCHAR(32) NOT NULL,
+	PRIMARY KEY(Id)
 );
 
 CREATE TABLE PRODUCT (
-    Id INTEGER NOT NULL,
-    Name VARCHAR(64)  NOT NULL UNIQUE,
-    Price DECIMAL(5,2),
-    Summary VARCHAR(512),
-    SourceOfSupply VARCHAR(32),
-    NumberOfProduct INTEGER,
-    PRIMARY KEY(Id)
+	Id INTEGER NOT NULL,
+	Name VARCHAR(64)  NOT NULL UNIQUE,
+	Price DECIMAL(5,2),
+	Summary VARCHAR(512),
+	SourceOfSupply VARCHAR(32),
+	NumberOfProduct INTEGER,
+	PRIMARY KEY(Id)
 );
 
 CREATE TABLE GAMECARD (
-    Pid       INTEGER      NOT NULL
-                CONSTRAINT Prod_FK_3 REFERENCES PRODUCT (Id) ON DELETE CASCADE
-                                                            ON UPDATE SET NULL,
-    Type      VARCHAR (32),
-    Rarety    VARCHAR (32),
-    BulkOrOne CHAR (1)
-
+	Pid INTEGER NOT NULL
+		CONSTRAINT Prod_FK_3 REFERENCES PRODUCT (Id)
+		ON DELETE CASCADE
+		ON UPDATE SET NULL,
+	Type VARCHAR (32),
+	Rarety VARCHAR (32),
+	BulkOrOne CHAR (1)
 );
-
 
 CREATE TABLE BOARDGAME (
-    Pid         INTEGER CONSTRAINT Prod_FK_4 REFERENCES PRODUCT (Id) ON DELETE CASCADE
-                                                                     ON UPDATE SET NULL
-                                                          NOT NULL,
-    PersonNum   VARCHAR(4),
-    AgeRestrict VARCHAR(4)
-
+	Pid INTEGER NOT NULL
+		CONSTRAINT Prod_FK_4 REFERENCES PRODUCT (Id)
+		ON DELETE CASCADE
+		ON UPDATE SET NULL,
+	PersonNum   VARCHAR(4),
+	AgeRestrict VARCHAR(4)
 );
-
-
 
 CREATE TABLE BUY (
-    Cid       INTEGER      NOT NULL
-                CONSTRAINT Cus_FK_1 REFERENCES CUSTOMER (Id) ON DELETE CASCADE
-                                                            ON UPDATE SET NULL,
-    Pid       INTEGER      NOT NULL
-                           CONSTRAINT Prod_FK_1 REFERENCES PRODUCT (Id) ON DELETE CASCADE
-                                                                        ON UPDATE SET NULL,
-    CargoName VARCHAR (16),
-    CardType  CHAR (1)
-
+	Cid INTEGER NOT NULL
+		CONSTRAINT Cus_FK_1 REFERENCES CUSTOMER (Id)
+		ON DELETE CASCADE
+		ON UPDATE SET NULL,
+	Pid INTEGER NOT NULL
+		CONSTRAINT Prod_FK_1 REFERENCES PRODUCT (Id)
+		ON DELETE CASCADE
+		ON UPDATE SET NULL,
+	CargoName VARCHAR (16),
+	CardType  CHAR (1)
 );
-
-
 
 CREATE TABLE REVIEW (
-    Cid     INTEGER      NOT NULL
-                         CONSTRAINT Cus_FK_2 REFERENCES CUSTOMER (Id) ON DELETE CASCADE
-                                                                      ON UPDATE SET NULL,
-    Pid     INTEGER      CONSTRAINT Prod_FK_2 REFERENCES PRODUCT (Id) ON DELETE CASCADE
-                                                                      ON UPDATE SET NULL
-                         NOT NULL,
-    Comment VARCHAR (64),
-    Time    TIMESTAMP
+	Cid INTEGER NOT NULL
+		CONSTRAINT Cus_FK_2 REFERENCES CUSTOMER (Id) 
+		ON DELETE CASCADE
+		ON UPDATE SET NULL,
+	Pid INTEGER NOT NULL,
+		CONSTRAINT Prod_FK_2 REFERENCES PRODUCT (Id)
+		ON DELETE CASCADE
+		ON UPDATE SET NULL,
+	Comment VARCHAR (64),
+	Time    TIMESTAMP
 );
 
+CREATE TABLE SHOPPINGLISTITEM (
+	Cid INTEGER NOT NULL
+		CONSTRAINT Shop_Cus_FK REFERENCES CUSTOMER (Id)
+		ON DELETE CASCADE
+		ON UPDATE SET NULL,
+	Pid INTEGER NOT NULL
+		CONSTRAINT Shop_Prod_FK REFERENCES PRODUCT (Id)
+		ON DELETE CASCADE
+		ON UPDATE SET NULL,
+	Quantity INTEGER
+);
